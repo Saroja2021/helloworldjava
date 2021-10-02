15d589400322cd86dd18254b6272da2a00b157c9
Saroja2021 <91406103+Saroja2021@users.noreply.github.com>
2021-10-02 12:30:17 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from Jenkins - testing of schedule - poll!!");
+    System.out.println("Hello World from Jenkins - testing of schedule - poll!!);
     }
 }
 
